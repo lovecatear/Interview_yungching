034d6ecae47509c1837abffe01e6d8c49324d83d
IRyS <saber83528+sourcetree@gmail.com>
2025-05-05 23:28:26 +0800

    feat: add database connection string configuration
    - Add connection string settings for database connectivity
    - Configure database connection parameters

--- ProductHub.Server/Program.cs
+++ ProductHub.Server/Program.cs
@@ -1,6 +1,11 @@
 using ProductHub.Common.Extensions;
+using ProductHub.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ProductHubContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ProductHubSqlConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerDocumentation();
